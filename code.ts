2f91e7e2526b0b90e63ad8a19bc22e84f95c91c2
Tung Phan <tungphanthanh247@gmail.com>
2025-06-11 22:19:39 +0700

    Read full attributes and show preview component

--- code.ts
+++ code.ts
@@ -1,37 +1,38 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+figma.showUI(__html__, { width: 400, height: 600 });
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+async function serializeNode(node: SceneNode): Promise<any> {
+  const previewBytes = await node.exportAsync({ format: "PNG", constraint: { type: "SCALE", value: 1 } });
+  const previewBase64 = figma.base64Encode(previewBytes);
 
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+  const serialized: any = {
+    id: node.id,
+    name: node.name,
+    type: node.type,
+    visible: node.visible,
+    width: node.width,
+    height: node.height,
+    x: node.x,
+    y: node.y,
+    rotation: (node as any).rotation ?? 0,
+    opacity: (node as any).opacity ?? 1,
+    preview: `data:image/png;base64,${previewBase64}`,
+    children: [],
+  };
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-shapes') {
-    // This plugin creates rectangles on the screen.
-    const numberOfRectangles = msg.count;
-
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < numberOfRectangles; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+  if ("children" in node && node.children.length > 0) {
+    const childPromises = node.children.map(serializeNode);
+    serialized.children = await Promise.all(childPromises);
   }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-};
+  return serialized;
+}
+
+figma.on("selectionchange", async () => {
+  const selection = figma.currentPage.selection[0];
+  if (selection) {
+    const data = await serializeNode(selection);
+    figma.ui.postMessage({ type: "NODE_TREE", data });
+  } else {
+    figma.ui.postMessage({ type: "NO_SELECTION" });
+  }
+});

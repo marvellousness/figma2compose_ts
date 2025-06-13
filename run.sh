c3bca7cb2f0603066476955e5f2a4602fb5721d0
Tung Phan <tungphan@asnet.com.vn>
2025-06-13 09:34:01 +0700

    Add build run command

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Fixed for webpack not found
+# npm install --save-dev webpack webpack-cli typescript ts-loader html-webpack-plugin inline-chunk-html-plugin
+
+# Build for production:
+# npm run build
+
+# Build for development with watch mode:
+npm run watch
